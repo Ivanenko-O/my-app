b257074e5402c7c7cabbc6f64ef5a9d7793ea771
olko <oles.ivanenko@gmail.com>
2017-07-26 14:58:35 +0300

    added dinamic(delete\choose checkbox)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component ({
+const todos = [
+  {
+    title: 'изучитть JS',
+    completed: true
+  },
+  {
+    title: 'изучитть nG2',
+    completed: false
+  },
+  {
+    title: 'написать SPA',
+    completed: false
+  },
+];
+
+@Component({
   moduleId: module.id,
   selector: 'app',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
- title = 'Angular 2Do';
+  title = 'Angular 2Do';
+  todos = todos; // свойство класса;
+
+  toggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  delete(todo) {
+    let index = this.todos.indexOf(todo);
+
+    if (index > -1) {
+      this.todos.splice(index, 1);
+    }
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,14 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-//
-// import { AppComponent } from './app.component';
-//
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 
 
-
-@NgModule ({
+@NgModule({
   imports: [BrowserModule],
   declarations: [AppComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 
